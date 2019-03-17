92f3293005e3c57e29a68a38d074c63a250e1df6
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-03-17 22:35:35 +0800

    add a merge fail method

---
+++ GitTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Git Test!");
+
+			MergeFail();
+		}
+
+		private static void MergeFail()
+		{
+			Console.WriteLine("Merge Fail");
+		}
+	}
+}
